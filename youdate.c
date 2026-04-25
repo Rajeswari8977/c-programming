d7dd522b27dc0f15fa653eb4b291c2f380b15617
Rajeswari8977 <allampallirajeswari9@gmail.com>
2026-04-25 12:38:43 +0530

    Add files via upload

---
+++ youdate.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int you,date;
+    printf("Enter the stylishness of you date\n");
+    scanf("%d%d",&you,&date);
+    if(you<=2||date<=2)
+    {
+        printf("no=0");
+    }
+    else if (you>=8||date>=8)
+    {
+        printf("yes=2");
+    }
+    else
+    {
+        printf("maybe=1");
+    }
+
+    return 0;
+}
